245ccc157b30337247b64fcc6e3fff53a43f7dff
rkaahean <ranjan31051997@gmail.com>
2020-04-30 22:32:03 -0700

    Completed Part a & Part b

--- pycuan/compmap/part_b.py
+++ pycuan/compmap/part_b.py
@@ -7,25 +7,25 @@ from sklearn.manifold import MDS
 cars_od = pd.read_csv("data/cars.dissimilarity.csv", sep=",", index_col=0)
 
 stress = []
-for k in range(1, 9):
-    mds = MDS(n_components=k, metric=True, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
+for k in range(1, 5):
+    mds = MDS(n_components=k, metric=False, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
     mdf_fit_out = mds.fit(cars_od)
     stress = stress + [mdf_fit_out.stress_]
 
-plt.plot(list(range(1, 9)), stress)
+plt.plot(list(range(1, 5)), stress)
 plt.xlabel("Values of k.")
 plt.ylabel("Stress.")
-plt.gcf().savefig('data/q1kchoose.png', dpi=500)
+plt.gcf().savefig('data/q2kchoose.png', dpi=500)
 # plt.show()
 
-print("Clearly, beyond k = 2, the values change barely, if at all. PART 1 Completed.\n")
+print("Clearly, beyond k = 3, the values change barely, if at all. PART 1 Completed.\n")
 
 """
 Part 2
 """
 print("Assuming k = 2.")
 
-mds = MDS(n_components=2, metric=True, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
+mds = MDS(n_components=2, metric=False, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
 mds_fit_out = mds.fit(cars_od)
 coods = np.array(mds_fit_out.embedding_)
 
@@ -38,7 +38,7 @@ for i in range(len(car_name)):
     plt.annotate(car_name[i], coods[i, :])
 plt.tight_layout()
 plt.axis('square')
-plt.gcf().savefig('data/q1plot.png', dpi=500)
+plt.gcf().savefig('data/q2plot.png', dpi=500)
 plt.show()
 
 
