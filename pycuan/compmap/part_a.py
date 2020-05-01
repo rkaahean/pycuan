04e3c8fff7c111f78709441b8ff1e9f472adb16a
rkaahean <ranjan31051997@gmail.com>
2020-04-30 19:14:09 -0700

    Completed part a. Issue pending with instability in scaling. Adding pad for text changes plot drastically.

---
+++ pycuan/compmap/part_a.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.manifold import MDS
+
+cars_od = pd.read_csv("data/cars.dissimilarity.csv", sep=",", index_col=0)
+cars_ar = pd.read_csv("data/cars.ar.csv", sep=",", index_col=0)
+
+stress = []
+for k in range(1, 9):
+    mds = MDS(n_components=k, metric=True, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
+    mdf_fit_out = mds.fit(cars_od)
+    stress = stress + [mdf_fit_out.stress_]
+
+plt.plot(list(range(1, 9)), stress)
+plt.xlabel("Values of k.")
+plt.ylabel("Stress.")
+plt.gcf().savefig('data/q1plot.png', dpi=500)
+# plt.show()
+
+print("Clearly, beyond k = 2, the values change barely, if at all. PART 1 Completed.\n")
+
+"""
+Part 2
+"""
+print("Assuming k = 2.")
+
+mds = MDS(n_components=2, metric=True, max_iter=1000, eps=1e-9, dissimilarity="precomputed", n_jobs=1)
+mds_fit_out = mds.fit(cars_od)
+coods = np.array(mds_fit_out.embedding_)
+
+car_name = list(cars_od.columns)
+
+plt.figure()
+plt.scatter(coods[:, 0], coods[:, 1])
+
+for i in range(len(car_name)):
+    plt.annotate(car_name[i], coods[i, :], [0.05 + x for x in coods[i, :]])
+plt.tight_layout()
+plt.axis('square')
+plt.show()
+
+
+
+
+
+
+
+
